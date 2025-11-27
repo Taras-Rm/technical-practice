0c0c317860c2fb5932d29d7f91536c721d5f8c5d
taras-rm <tarasrom25@gmail.com>
2025-11-26 21:01:33 -0500

    add delete and update command

--- task-tracker/cli/cli.go
+++ task-tracker/cli/cli.go
@@ -2,6 +2,7 @@ package cli
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/Taras-Rm/technical-practice/task-tracker/db"
 )
@@ -21,9 +22,15 @@ func (c *CLI) ParseCommand(args []string) error {
 		return fmt.Errorf("invalid command")
 	}
 
+	args = args[1:]
+
 	switch args[0] {
 	case "add":
-		return c.handleAdd(args[1:])
+		return c.handleAdd(args)
+	case "delete":
+		return c.handleDelete(args)
+	case "update":
+		return c.handleUpdate(args)
 	default:
 		fmt.Println("Wrong command.")
 	}
@@ -44,6 +51,48 @@ func (c *CLI) handleAdd(args []string) error {
 	}
 
 	fmt.Printf("Task added successfully (ID: %d)", task.Id)
+
+	return nil
+}
+
+func (c *CLI) handleDelete(args []string) error {
+	if len(args) == 0 {
+		return fmt.Errorf("id required")
+	}
+
+	id, err := strconv.Atoi(args[0])
+	if err != nil {
+		return err
+	}
+
+	err = c.taskStore.Delete(id)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Task added deleted (ID: %d)", id)
+
+	return nil
+}
+
+func (c *CLI) handleUpdate(args []string) error {
+	if len(args) < 3 {
+		return fmt.Errorf("wrong command")
+	}
+
+	id, err := strconv.Atoi(args[0])
+	if err != nil {
+		return err
+	}
+
+	description := args[1]
+
+	err = c.taskStore.Update(id, description)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Task updated (ID: %d)", id)
 
 	return nil
 }
